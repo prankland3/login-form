d8696b0b0e25f1f9dc5bf6948985d2b8f67645df
prankland3 <thibaut.verbeke2005@hotmail.com>
2024-06-15 14:55:10 +0200

    commit

--- auth/register.py
+++ auth/register.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 import sqlite3
 import hashlib
@@ -6,27 +7,33 @@ import hashlib
 def register(username, password, password_repeat, email, firstName, lastName):
     if password == "" or password_repeat == "" or email == "" or firstName == "" or lastName == "":
         empty_err = "Some of the criteria isn't filled in please check them."
-        return render_template("register.html"), print(empty_err)
+        return render_template("register.html", err = empty_err)
+    
     elif password != password_repeat:
         repeat_err = "Password didn't match. Please try again."
-        return render_template("register.html"), print(repeat_err)
+        return render_template("register.html", err = repeat_err)
+    
     elif len(password) < 8:
         password_len_err = "Password must be at least 8 characters long."
-        return render_template("register.html"), password_len_err
+        return render_template("register.html", err = password_len_err)
+    
     elif not any(char.islower() for char in password):
         password_lower_err = "Password must contain lower case letters."
-        return render_template("register.html"), print(password_lower_err)
+        return render_template("register.html", err = password_lower_err)
+    
     elif not any(char.isupper() for char in password):
         password_upper_err = "Password must contain upper case letters."
-        return render_template("register.html"), print(password_upper_err)
+        return render_template("register.html", err = password_upper_err)
+    
     elif not any(char.isdigit() for char in password):
         password_number_err = "Password must contain numbers."
-        return render_template("register.html"), print(password_number_err)
+        return render_template("register.html", err = password_number_err)
+    
     else:
         password = hashlib.md5(password.encode()).hexdigest()
         insert_user(username, password, email, firstName, lastName)
         success_sign_up = "sign up is successful.<br> you can now try to login"
-        return render_template("login.html"), print(success_sign_up)
+        return render_template("login.html", err = success_sign_up)
 
 
 def insert_user(username, password, email, firstName, lastName):
