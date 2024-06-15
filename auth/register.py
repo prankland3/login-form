d97dbb54f34799abd1f89cfb428f4a352881c478
Sibren Wieme <sibren.wieme@ugent.be>
2024-06-15 15:29:14 +0200

    Refactoring register.py

--- auth/register.py
+++ auth/register.py
@@ -5,36 +5,40 @@ import hashlib
 
 
 def register(username, password, password_repeat, email, firstName, lastName):
+    error_messages = []
  
     if password == "" or password_repeat == "" or email == "" or firstName == "" or lastName == "":
         empty_err = "Some of the criteria isn't filled in please check them."
         return render_template("register.html", err = empty_err)
-    
-    elif password != password_repeat:
-        repeat_err = "Password didn't match. Please try again."
-        return render_template("register.html", err = repeat_err)
-    
-    elif len(password) < 8:
-        password_len_err = "Password must be at least 8 characters long."
-        return render_template("register.html", err = password_len_err)
-    
-    elif not any(char.islower() for char in password):
-        password_lower_err = "Password must contain lower case letters."
-        return render_template("register.html", err = password_lower_err)
-    
-    elif not any(char.isupper() for char in password):
-        password_upper_err = "Password must contain upper case letters."
-        return render_template("register.html", err = password_upper_err)
-    
-    elif not any(char.isdigit() for char in password):
-        password_number_err = "Password must contain numbers."
-        return render_template("register.html", err = password_number_err)
-    
+
     else:
+        if password != password_repeat:
+            repeat_err = "Password didn't match. Please try again."
+            error_messages.append(repeat_err)
+
+        if len(password) < 8:
+            password_len_err = "Password must be at least 8 characters long."
+            error_messages.append(password_len_err)
+
+        if not any(char.islower() for char in password):
+            password_lower_err = "Password must contain lower case letters."
+            error_messages.append(password_lower_err)
+
+        if not any(char.isupper() for char in password):
+            password_upper_err = "Password must contain upper case letters."
+            error_messages.append(password_upper_err)
+
+        if not any(char.isdigit() for char in password):
+            password_number_err = "Password must contain numbers."
+            error_messages.append(password_number_err)
+
+    if len(error_messages) == 0:
         password = hashlib.md5(password.encode()).hexdigest()
         insert_user(username, password, email, firstName, lastName)
         success_sign_up = "sign up is successful.<br> you can now try to login"
         return render_template("login.html", err = success_sign_up)
+    else:
+        return render_template("register.html", err = error_messages)
 
 
 def insert_user(username, password, email, firstName, lastName):
